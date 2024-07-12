2639e88217a32ee2a70ba3d71b79ded9935407e1
LauvelM <lauvelm99@gmail.com>
2024-07-11 21:38:27 -0500

    test

--- Models/User.cs
+++ Models/User.cs
@@ -2,6 +2,7 @@ namespace CVManager.Models
 {
     public class User
     {
+        
         public int? Id { get; set; }
         public string? Name { get; set; }
         public string? Age { get; set; }
